7b6c35a6cb022543447f05f5fb2a207d2c216b09
Suman <suman_grg@outlook.com>
2021-07-22 18:35:59 +0000

    Functions for creating post

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,24 @@
-from application import app
+from application import app, db
 from flask import redirect, request, render_template, url_for
+from application.forms import BasicForm
+from application.models import Post, Comment
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('layout.html')
+    all_post = Post.query.all()
+
+    return render_template("home.html", all_post = all_post)
+
+@app.route('/post', methods = ['GET', 'POST'])
+def post():
+    form = BasicForm()
+
+    if request.method == 'POST':
+        new_post = Post(content = form.post.data)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('create.html', form = form)
+
