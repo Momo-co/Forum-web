794101f7270ae841388c14e90e6567d13a751d9a
Suman <suman_grg@outlook.com>
2021-07-23 14:02:56 +0000

    Added additional flask forms

--- application/forms.py
+++ application/forms.py
@@ -3,4 +3,10 @@ from wtforms import StringField, SubmitField
 
 class BasicForm(FlaskForm):
     post = StringField('Forum')
+    comment = StringField('Comment')
+    edit_post = StringField('Edit Post')
+    modify_comment = StringField('Edit Comment')
+    submit_comment = SubmitField('Enter Comment')
     submit_post = SubmitField('Post')
+    change_post = SubmitField('Change Post')
+    change_comment = SubmitField('Change Comment')
